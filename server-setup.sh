f412e9a6543f09120930d01b7f82359636e2ec66
Peter <tutorials@alexabible.com>
2018-05-07 17:27:28 +0100

    php-fpm done

--- server-setup.sh
+++ server-setup.sh
@@ -2,3 +2,6 @@
 
 #First update the system
 yum upgrade -y
+yum -y install wget git
+yum install epel* -y
+yum upgrade -y
---
+++ server-setup/02-php-fpm.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "$(tput setaf 1)#########################################################################"
+echo "$(tput setaf 2)Installing PHP-FPM$(tput setaf 1)"
+echo "#########################################################################$(tput setaf 7)"
+
+#Add the remi repository
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
+yum install yum-utils -y
+yum-config-manager --enable remi-php72
+yum update -y
+
+#Install PHP-FPM and additional modules
+yum -y --enablerepo=remi,remi-php72 install php-fpm php-common
+yum -y --enablerepo=remi,remi-php72 install php-opcache php-pecl-apcu php-cli php-pdo php-pgsql php-pecl-memcache php-pecl-memcached php-gd php-mbstring php-mcrypt php-xml
+
+enableService php-fpm
+
+#Get Composer
+wget https://getcomposer.org/installer
+mv installer composer-setup.php
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+rm -rf composer-setup.php
