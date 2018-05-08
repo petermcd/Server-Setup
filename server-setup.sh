76d4572eefd44016904b83bebd83f26a539a3427
Peter <tutorials@alexabible.com>
2018-05-09 00:25:56 +0100

    adding header, capture and report ip

--- server-setup.sh
+++ server-setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo "$(tput setaf 1)#########################################################################"
+echo "$(tput setaf 2)Installing Server$(tput setaf 1)"
+echo "#########################################################################$(tput setaf 7)"
+
 #First update the system
 yum upgrade -y
 yum -y install wget git
@@ -13,7 +17,23 @@ yum install firewalld -y
 enableService firewalld
 firewall-cmd --reload
 
+ipString=`hostname -I`
+ipArray=$(echo $ipString | tr " " "\n")
+ip4=""
+ip6=""
+for ip in $ipArray
+do
+        if [[ $ip =~ ":" ]]
+        then
+                ip6=$ip
+        else
+                ip4=$ip
+        fi
+done
 echo "Server: " `hostname -f` >> ./emailFile.txt
+echo
+echo "IPv4: " $ip4
+echo "IPv6: " $ip6
 
 fileString=`ls server-setup/*.sh`
 #echo $fileString
